262dc159bea5df832db41ba233ea84d8abc5c2a7
Vaibhav0032 <100957365+Vaibhav0032@users.noreply.github.com>
2024-01-18 18:19:26 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 320032 //const is used for permanent value that cannot be changed
+let accountEmail = "vgarg@gmail.com"
+var accountPassword = "12345"
+accountCity = "Himachal"
+
+/*
+Perfer not to use var instead of it use only let
+because of issue in block 
+*/
+
+// accountId = 2
+
+console.log(accountId);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all JS code in new version
+
+console.log("popUp")
+//alert("hello")
+
+let name ="hitesh"
+let age = 23
+let isLoggedi = "false"
+
+// number => 2 to power 53
+//bigint 
+// string ==> ""
+// boolean ==> true/false
+// null ==> standalone value
+// undefined ==>
+// symbol 
+
+// object
+
+console.log(typeof age);
